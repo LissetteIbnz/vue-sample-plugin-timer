8a682c90f2c545fd110d839dad27226d956d49e6
Sara Lissette Luis Ibáñez <sara.lissette@ultebra.eu>
2018-12-11 15:40:47 +0000

    Remove comments

--- src/shims-plugins.d.ts
+++ src/shims-plugins.d.ts
@@ -1,15 +1,7 @@
 import Vue, { VueConstructor } from 'vue';
 
-// declare module 'vue/types/vue' {
-//   // Global properties can be declared
-//   // on the `VueConstructor` interface
-//   interface VueConstructor {
-//     $options: any
-//   }
-// }
 declare module 'vue/types/options' {
   interface ComponentOptions<V extends Vue> {
     timers?: any;
-    // $options?: any;
   }
 }
